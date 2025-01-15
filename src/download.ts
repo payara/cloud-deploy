400b2eb55a222a8906739966cef2fb9fba4c8ab8
arieki <arie.kiswanto@payara.fish>
2025-01-15 19:36:19 +0700

    CLOUD-2914 fixed download

--- src/download.ts
+++ src/download.ts
@@ -1,23 +1,20 @@
 import * as axios from 'axios';
 import * as fs from 'fs';
 
-export async function downloadPclJarFile(url: string, outputPath: string): Promise<void> {
-    const writer = fs.createWriteStream(outputPath);
-
+export async function downloadPclJarFile(url: string, outputPath: string) {
     try {
-        const response = await axios.default.get(url, { responseType: 'stream' });
+        const writer = fs.createWriteStream(outputPath);
+        const response = await axios.default({
+            url,
+            method: 'GET',
+            responseType: 'stream',
+        });
         response.data.pipe(writer);
-
         return new Promise((resolve, reject) => {
             writer.on('finish', resolve);
-            writer.on('error', (err) => {
-                // Remove partially downloaded file on error
-                fs.unlink(outputPath, () => reject(err));
-            });
+            writer.on('error', reject);
         });
     } catch (error) {
         throw new Error(`Failed to download PCL binaries: ${(error as Error).message}`);
-    } finally {
-        writer.close();
     }
 }
